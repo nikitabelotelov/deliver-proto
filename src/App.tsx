847d8e9b7ad3b7b473f97fe76985e1c8ec9b0dc4
nikita belotelov <nikbelotelov@gmail.com>
2020-06-13 15:19:48 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// https://developers.google.com/web/updates/2011/08/Saving-generated-files-on-the-client-side
+function App() {
+  return (
+    <div>
+      Hello
+    </div>
+  );
+}
+
+export default App;
